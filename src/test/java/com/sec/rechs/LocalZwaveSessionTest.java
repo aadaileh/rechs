c46f8ad9e0b4863b1866d6f299060b0ef3ac513b
aadaileh <ahmed.adaileh@gmail.com>
2018-07-07 01:13:10 +0200

    Successfully integrate the zwave jar as external library :)

--- src/test/java/com/sec/rechs/LocalZwaveSessionTest.java
+++ src/test/java/com/sec/rechs/LocalZwaveSessionTest.java
@@ -1,6 +1,5 @@
 package com.sec.rechs;
 
-
 import com.oberasoftware.base.event.EventHandler;
 import com.oberasoftware.base.event.EventSubscribe;
 import com.oberasoftware.home.zwave.api.ZWaveSession;
@@ -8,24 +7,13 @@ import com.oberasoftware.home.zwave.api.actions.SwitchAction;
 import com.oberasoftware.home.zwave.api.actions.devices.MeterGetAction;
 import com.oberasoftware.home.zwave.api.events.ZWaveEvent;
 import com.oberasoftware.home.zwave.api.events.devices.DeviceSensorEvent;
-import com.oberasoftware.home.zwave.api.events.devices.MeterEvent;
 import com.oberasoftware.home.zwave.api.events.devices.SwitchEvent;
 import com.oberasoftware.home.zwave.api.events.devices.SwitchLevelEvent;
 import com.oberasoftware.home.zwave.core.ZWaveNode;
 import com.oberasoftware.home.zwave.exceptions.HomeAutomationException;
 import com.oberasoftware.home.zwave.local.LocalZwaveSession;
-import com.sec.rechs.exception.ResourceNotFoundException;
-import com.sec.rechs.model.Appliance;
-import com.sec.rechs.model.Measurment;
-import com.sec.rechs.repository.ApplianceRepository;
-import com.sec.rechs.repository.MeasurmentRepository;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.math.BigDecimal;
 import java.util.List;
 
 import static com.google.common.util.concurrent.Uninterruptibles.sleepUninterruptibly;
@@ -33,43 +21,34 @@ import static com.oberasoftware.home.zwave.api.messages.types.MeterScale.Electri
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.slf4j.LoggerFactory.getLogger;
 
-@RestController
-@RequestMapping("/api")
-public class ApplianceController {
+/**
+ * @author renarj
+ */
+public class LocalZwaveSessionTest {
+    private static final Logger LOG = getLogger(LocalZwaveSessionTest.class);
 
-    private static final Logger LOG = getLogger(ApplianceController.class);
+    //private static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
 
-
-    @Autowired
-    MeasurmentRepository measurmentRepository;
-
-    @Autowired
-    ApplianceRepository applianceRepository;
-
-    // Get data from node
-    @GetMapping("/nodes/{nodeId}/data")
-    public String getNodeData() {
+    public static void main(String[] args) {
         LOG.info("Starting Local ZWAVE App");
         try {
             doZwaveStuff();
         } catch (HomeAutomationException e) {
             LOG.error("", e);
         }
-
-        return "x";
     }
 
     /**
      * Initialises the binding. This is called after the 'updated' method
      * has been called and all configuration has been passed.
      */
-    public void doZwaveStuff() throws HomeAutomationException {
+    public static void doZwaveStuff() throws HomeAutomationException {
         LOG.debug("Application startup");
         ZWaveSession s = new LocalZwaveSession();
 
         s.connect();
 
-//        List<ZWaveNode> nodes1 = s.getDeviceManager().getNodes();
+        List<ZWaveNode> nodes1 = s.getDeviceManager().getNodes();
 
 
 //        while(!s.isNetworkReady()) {
@@ -152,27 +131,11 @@ public class ApplianceController {
 //
     }
 
-    public class MyEventListener implements EventHandler {
-
-        @Autowired
-        MeasurmentRepository measurmentRepository2;
-
-        @Autowired
-        ApplianceRepository applianceRepository2;
+    public static class MyEventListener implements EventHandler {
 
         @EventSubscribe
         public void receive(ZWaveEvent event) throws Exception {
             LOG.info("Received an event (customized): {}", event);
-
-            BigDecimal value = ((MeterEvent) event).getValue();
-            Measurment measurment = new Measurment();
-            applianceRepository2.findById(3L).map(appliance -> {
-                measurment.setAppliance(appliance);
-                measurment.setKwh(123.1);
-                return measurmentRepository2.save(measurment);
-            }).orElseThrow(() -> new ResourceNotFoundException("Appliance", "id", 3));
-
-            //return event.getValue();
         }
 
         @EventSubscribe
@@ -191,55 +154,5 @@ public class ApplianceController {
 
             LOG.info("Received a switch event: {}", event);
         }
-    }
-
-
-    // Get All Appliances
-    @GetMapping("/appliances")
-    public List<Appliance> getAllAppliances() {
-        return applianceRepository.findAll();
-    }
-
-    // Create a new Appliance
-    @PostMapping("/appliances")
-    public Appliance createAppliance(@Valid @RequestBody Appliance appliance) {
-
-        //HardwareManager hardwareManager = new HardwareManager();
-        //ZWaveDriver zWaveDriver = new ZWaveDriver();
-
-        return applianceRepository.save(appliance);
-    }
-
-    // Get a Single Appliance
-    @GetMapping("/appliances/{id}")
-    public Appliance getApplianceById(@PathVariable(value = "id") Long applianceId) {
-        return applianceRepository.findById(applianceId)
-                .orElseThrow(() -> new ResourceNotFoundException("Appliance", "id", applianceId));
-    }
-
-    // Update a Appliance
-    @PutMapping("/appliances/{id}")
-    public Appliance updateAppliance(@PathVariable(value = "id") Long applianceId,
-                                @Valid @RequestBody Appliance applianceDetails) {
-
-        Appliance appliance = applianceRepository.findById(applianceId)
-                .orElseThrow(() -> new ResourceNotFoundException("Appliance", "id", applianceId));
-
-        appliance.setSystemName(applianceDetails.getSystemName());
-        appliance.setLabel(applianceDetails.getLabel());
-
-        Appliance updatedAppliance = applianceRepository.save(appliance);
-        return updatedAppliance;
-    }
-
-    // Delete a Appliance
-    @DeleteMapping("/appliances/{id}")
-    public ResponseEntity<?> deleteAppliance(@PathVariable(value = "id") Long applianceId) {
-        Appliance appliance = applianceRepository.findById(applianceId)
-                .orElseThrow(() -> new ResourceNotFoundException("Appliance", "id", applianceId));
-
-        applianceRepository.delete(appliance);
-
-        return ResponseEntity.ok().build();
     }
 }
